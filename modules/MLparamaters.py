801115472f7501ea5a8e7605c1012d03d749cafa
tamae1111 <taoyjk@gmail.com>
2024-01-09 18:49:42 +0900

    stable training in sagemaker instance ver

--- modules/MLparamaters.py
+++ modules/MLparamaters.py
@@ -25,10 +25,12 @@ def getMLParamaters():
 
     result = DictDotNotation()
 
-
     # sagemakerの環境変数を支える場合は、s3も使うため、連動してこのフラグをTrueにする
+    isCUDAAvailable:bool =  torch.cuda.is_available()
+
     useS3:bool = bool(os.environ.get('SM_CHANNEL_TRAINING'))
 
+    # SagemakerAPIで立ち上げたインスタンスの場合
     if useS3:
         # sagemakerの環境変数から取得
         input_data_dir = os.environ.get('SM_CHANNEL_TRAINING')
@@ -38,9 +40,19 @@ def getMLParamaters():
         model_path = os.environ.get('SM_MODEL_DIR') + "/model.pt"
         # "/opt/ml/model"
 
-
-    else:
+    # notebookインスタンスの場合
+    elif isCUDAAvailable:
         basePath = '/home/ec2-user/SageMaker/maskrcnn'
+        middle_path = "/custombdd"
+        bdd_xml= basePath+ middle_path + "/xml"
+        bdd_img= basePath+ middle_path + "/img"
+        test_path= basePath+ middle_path + "/test"
+        model_path = basePath +'/models/model.pt'
+        # CUDAが使えない状態（完全ローカル）
+    
+    # 完全ローカルの場合
+    else:
+        basePath = '/Users/tamae/gitdir/maskrcnn'
         middle_path = "/custombdd"
         bdd_xml= basePath+ middle_path + "/xml"
         bdd_img= basePath+ middle_path + "/img"
@@ -48,11 +60,10 @@ def getMLParamaters():
         model_path = basePath +'/models/model.pt'
 
 
-
     dataset_class = ['person','vehicle','c_vehicle2','c_vehicle3','tank_lorry','truck','c_vehicle','c_vehicle4']
     colors = ((0,0,0),(255,0,0),(0,255,0),(0,0,255),(100,255,100),(100,100,255),(255,255,0),(255,0,255),(0,255,255))
 
-    divice = torch.device('cuda') if torch.cuda.is_available() else torch.device('cpu') 
+    divice = torch.device('cuda') if isCUDAAvailable else torch.device('cpu') 
 
     #ハイパーパラメータの指定
     epochs = 20
